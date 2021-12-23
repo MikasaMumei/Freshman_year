2fc12c7beec7d93383221e64bfbd1445327708f5
Caohhuxii <46293442+MikasaMumei@users.noreply.github.com>
2021-12-23 14:38:17 +0800

    Update 1011.f90

--- GitHub_FortranHomework/1011.f90
+++ GitHub_FortranHomework/1011.f90
@@ -1,4 +1,4 @@
-Program Quadratic_Equation_of_One_Variable_Simple
+Program Quadratic_Equation_of_One_Variable_Simple   !一段简单的解一元二次方程的代码，没有考虑非一元二次方程的情况。
 implicit none
     real(8):: delta , x1 , x2 , a , b , c
     
@@ -11,7 +11,7 @@ implicit none
      else if (delta == 0 ) then
         x1 = -b / 2.*a
         write(*,100) 'r1=' , x1 , 'r2=' , x1
-100 format('' , A3 , F7.2 , / , A3 , F7.2)
+100 format('' , A3 , F7.2 , / , A3 , F7.2)        !一种格式化输出
     else 
         write(*,200)'No real roots!'
 200 format('' , A14)
